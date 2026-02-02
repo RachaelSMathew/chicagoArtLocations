c5cc5e482b42538d90c4af02951523da4f5e377d
Rachael Mathew <rachaelmathew1@gmail.com>
2026-02-01 21:11:41 -0500

    updated tests

--- chicagoArtLocationsBE/test_material/kd_tree_validity_test.py
+++ chicagoArtLocationsBE/test_material/kd_tree_validity_test.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from haversine import haversine
 import json
 import sys
@@ -8,7 +9,9 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(parent_dir)
 import collectPointsKDTree
 
-""" 
+"""
+We're not checking for the order of the K closest points, just verifying that the K closest points are correct
+
 1. a test where the points have more vairance in longitude
 2. a test where the points have more vairance in latitude
 3. a test where the points are all within two miles of another point 
@@ -31,20 +34,47 @@ def testKDTree():
         kdTree = collectPointsKDTree.createKDTree(
             points, collectPointsKDTree.whichAxisSplitShouldBe(points)
         )
+        dist_of_chicago_to_points = []
+        dist_of_south_carolina_to_points = []
+
+        for point in points:
+            dist_of_chicago_to_points.append(
+                {
+                    "id": point["mural_registration_id"],
+                    "distance": haversine(
+                        (point["latitude"], point["longitude"]), (47.8832, -87.6424)
+                    ),
+                }
+            )
+            dist_of_south_carolina_to_points.append(
+                {
+                    "id": point["mural_registration_id"],
+                    "distance": haversine(
+                        (point["latitude"], point["longitude"]), (34.0522, -81.0559)
+                    ),
+                }
+            )
+        ## sort dist_of_chicago_to_points and dist_of_south_carolina_to_points by distance
+        dist_of_chicago_to_points.sort(key=lambda x: x["distance"])
+        dist_of_south_carolina_to_points.sort(key=lambda x: x["distance"])
+        dist_of_chicago_to_points = [
+            d["id"] for d in dist_of_chicago_to_points[:50]
+        ].sort()
+        dist_of_south_carolina_to_points = [
+            d["id"] for d in dist_of_south_carolina_to_points[:50]
+        ].sort()
+
         coord_in_chicago = {"latitude": 47.8832, "longitude": -87.6424}
         coord_in_south_carolina = {"latitude": 34.0522, "longitude": -81.0559}
         for coord in [coord_in_chicago, coord_in_south_carolina]:
             closestPoints = collectPointsKDTree.newsearch(
-                coord["latitude"], coord["longitude"], 0, len(points)
+                coord["latitude"], coord["longitude"], 0, 50
             )
-            startingPoint = closestPoints[0]
-            fullyConsectutive = True
-            for i in range(1, len(points)):
-                if startingPoint[0] > closestPoints[i][0]:
-                    print("Starting point is not closer than the ${i}th closest point")
-                    fullyConsectutive = False
-                    break
-                startingPoint = closestPoints[i]
-            if fullyConsectutive:
-                print("All", len(points), "points are consecutive")
-            assert fullyConsectutive == True
+            closestPoints_ids = [
+                point[1]["mural_registration_id"] for point in closestPoints
+            ].sort()
+            assert closestPoints_ids == (
+                dist_of_chicago_to_points
+                if coord == coord_in_chicago
+                else dist_of_south_carolina_to_points
+            )
