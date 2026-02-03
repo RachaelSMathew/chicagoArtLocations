ed4728f2ac4e62e1799b4bf8c095b94cda0bd1b3
Rachael Mathew <rachaelmathew1@gmail.com>
2026-02-02 21:22:50 -0500

    removed opensearch usage when in production env

--- chicagoArtLocationsBE/index.py
+++ chicagoArtLocationsBE/index.py
@@ -2,12 +2,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import sys
-sys.path.append('.') ## appends . to end of PYTHONPATH
 from collectPointsKDTree import newsearch, defaultFunc
 import time
-app = FastAPI()
+import os
 from opensearch import createIndex, searchIndex
 import copy
+
+sys.path.append(".")  ## appends . to end of PYTHONPATH
+app = FastAPI()
 
 # Enable CORS
 app.add_middleware(
@@ -17,29 +19,53 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 async def startup_event():
     """Initialize KD-tree when FastAPI starts"""
-    createIndex()
+    if os.getenv("NODE_ENV") == "production":
+        createIndex()  # only create opensearch index in production
     defaultFunc()  # Runs once at startup, after server/uvicorn.run starts
+
 
 # This runs EVERY time someone visits /api/search
 @app.get("/newsearch/")
-async def search(lat: float, long: float, minDistance: float = 0, searchQuery: str = ''):
+async def search(
+    lat: float, long: float, minDistance: float = 0, searchQuery: str = ""
+):
     start_time = time.time()
-    opensearchReturn = searchIndex(searchQuery).get('hits', []).get('hits', [])
+    opensearchReturn = searchIndex(searchQuery).get("hits", []).get("hits", [])
     results = newsearch(lat, long, minDistance)
     resultsFurtherFiltered = []
-    for i in opensearchReturn:
+
+    if os.getenv("NODE_ENV") == "production":
         for result in results:
-            if(result[1]['mural_registration_id'] == i.get('_id')):
+            resultConcatenated = (
+                result[1]["artwork_title"]
+                + " "
+                + result[1]["artist_name"]
+                + " "
+                + result[1]["artwork_description"]
+                + " "
+                + result[1]["street_address"]
+            )
+            if searchQuery.lower() in (resultConcatenated).lower():
                 resultsFurtherFiltered.append(copy.deepcopy(result))
+    else:
+        for i in opensearchReturn:
+            for result in results:
+                if result[1]["mural_registration_id"] == i.get("_id"):
+                    resultsFurtherFiltered.append(copy.deepcopy(result))
     return {
         "results": results if searchQuery == "" else resultsFurtherFiltered,
         "count": len(results if searchQuery == "" else resultsFurtherFiltered),
-        "time_seconds": time.time() - start_time
+        "time_seconds": time.time() - start_time,
     }
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) # This starts server(does not happen at every request), blocks forever
+
+    uvicorn.run(
+        app, host="0.0.0.0", port=8000
+    )  # This starts server(does not happen at every request), blocks forever
